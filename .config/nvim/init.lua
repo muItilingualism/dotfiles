88be3dc0b399bbe5746dd484507f25fc3ab49be0
muItilingualism <sebhalfdan@gmail.com>
2024-03-14 21:56:31 +0100

    Move plugins to subfolder

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,37 +21,5 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-    {
-        "catppuccin/nvim",
-        lazy = false,
-        name = "catppuccin",
-        priority = 1000
-    },
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.5',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" }
-}
-
 local opts = {}
-
-require("lazy").setup(plugins, opts)
-
--- colorscheme
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
-
--- telescope
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
-
--- treesitter
-local configs = require("nvim-treesitter.configs")
-configs.setup({
-    ensure_installed = { "c", "lua", "vim", "rust", "typescript", "javascript", "html" },
-    highlight = { enable = true },
-    indent = { enable = true },
-})
+require("lazy").setup("plugins")
