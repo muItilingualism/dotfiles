f67dcb1ab879942b6072ab811247c7b18c31deb1
muItilingualism <sebhalfdan@gmail.com>
2024-03-15 20:04:03 +0100

    Remove arrow keys in insert mode
    Replaced with C-hjkl for quick navigation

--- .config/nvim/lua/vim-bindings.lua
+++ .config/nvim/lua/vim-bindings.lua
@@ -6,3 +6,13 @@ vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+vim.keymap.set('i', '<left>', '<Nop>')
+vim.keymap.set('i', '<right>', '<Nop>')
+vim.keymap.set('i', '<up>', '<Nop>')
+vim.keymap.set('i', '<down>', '<Nop>')
+
+vim.keymap.set('i', '<C-h>', '<left>')
+vim.keymap.set('i', '<C-l>', '<right>')
+vim.keymap.set('i', '<C-k>', '<up>')
+vim.keymap.set('i', '<C-j>', '<down>')
