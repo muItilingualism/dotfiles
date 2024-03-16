da2f79a3c8db2b1159dd7ab1a8aa1bd0c143a23d
muItilingualism <sebhalfdan@gmail.com>
2024-03-16 17:00:50 +0100

    Format code

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -39,10 +39,10 @@ vim.opt.colorcolumn = "80"
 vim.opt.smartindent = true
 
 -- highlight when yanking
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
